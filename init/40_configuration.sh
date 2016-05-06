906f0cb954aac2587829c9940e98df3dc0549fed
unknown <alex.wilkinson.admin@CST008570.kmh.nhs.uk>
2016-05-06 08:07:43 +0100

    no message

--- init/40_configuration.sh
+++ init/40_configuration.sh
@@ -8,8 +8,8 @@ sql="/config/nadekobot.sqlite"
 if [ -f "$sql" ] ; then echo "SQLite DB exists" ; else echo "" > /config/nadekobot.sqlite ;fi
 
 #Remove Built in
-rm /root/NadekoBot/credentials.json
-rm /root/NadekoBot/data/nadekobot.sqlite
+#rm /root/NadekoBot/credentials.json
+#rm /root/NadekoBot/data/nadekobot.sqlite
 
 #Adding Config file sym links
 ln -s /config/credentials.json /root/NadekoBot/credentials.json
