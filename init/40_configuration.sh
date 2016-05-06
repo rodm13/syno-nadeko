bf9e98592d22b72e7fc002794c3e25d8430df4b7
unknown <alex.wilkinson.admin@CST008570.kmh.nhs.uk>
2016-05-06 08:26:52 +0100

    no message

--- init/40_configuration.sh
+++ init/40_configuration.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 #Test Configuration
 
-cred="/config/credentials.json"
-if [ -f "$cred" ] ; then echo "Credentials exist" ; else cp /root/NadekoBot/credentials_example.json /config/credentials.json ; fi
+if test -f "/config/credentials.json"; then echo "Creds Exist"; else cp /root/NadekoBot/credentials_example.json /config/credentials.json; fi
+if test -f "/config/nadekobot.sqlite"; then echo "SQlite Exist"; else echo "" > /config/nadekobot.sqlite; fi
+if test -f "/config/config.json"; then echo "Config Exist"; else cp /root/NadekoBot/data/config_example.json /config/config.json; fi
 
-sql="/config/nadekobot.sqlite"
-if [ -f "$sql" ] ; then echo "SQLite DB exists" ; else echo "" > /config/nadekobot.sqlite ;fi
 
 #Remove Built in
-#rm /root/NadekoBot/credentials.json
-#rm /root/NadekoBot/data/nadekobot.sqlite
+rm /root/NadekoBot/credentials.json
+rm /root/NadekoBot/data/nadekobot.sqlite
+rm /root/NadekoBot/data/config_example.json
 
 #Adding Config file sym links
 ln -s /config/credentials.json /root/NadekoBot/credentials.json
 ln -s /config/nadekobot.sqlite /root/NadekoBot/data/nadekobot.sqlite
+ln -s /config/config.json /root/NadekoBot/data/config.json
