edb9bd23fe964e9187d530b5880acefd6f841f91
Poag <Poag@users.noreply.github.com>
2016-05-06 17:23:35 +0100

    Update 40_nadekoinstall.sh
    Cleanup old installs

--- init/40_nadekoinstall.sh
+++ init/40_nadekoinstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ##Install NadekoBot
 
-#Backup Existing NadekoBot
-rsync /root/NadekoBot /root/NadekoBot.bak
+#Cleanup Old
+rm -rf /root/NadekoBot
 
 #Get NadekoBot Latest
 curl -s https://api.github.com/repos/Kwoth/NadekoBot/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4|wget -i - -O /root/NadekoBot.latest.zip &&\
