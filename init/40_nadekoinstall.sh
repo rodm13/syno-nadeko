616bcfcf5d19172ed7cbb6645e6a2bef8af5eba9
Poag <Poag@users.noreply.github.com>
2016-08-12 11:48:33 +0100

    Added version variable. Defaults to Prod releases

--- init/40_nadekoinstall.sh
+++ init/40_nadekoinstall.sh
@@ -3,8 +3,13 @@
 #Cleanup Old
 rm -rf /root/NadekoBot
 
-#Get NadekoBot Latest
-curl -s https://api.github.com/repos/Kwoth/NadekoBot/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4|wget -i - -O /root/NadekoBot.latest.zip &&\
+#Get NadekoBot Version
+if [[ "$VERSION" = dev]]; then
+	curl -s https://api.github.com/repos/Kwoth/NadekoBot/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4|wget -i - -O /root/NadekoBot.latest.zip &&\
+else
+	curl -s https://api.github.com/repos/Kwoth/NadekoBot/releases/latest | grep browser_download_url | head -n 1 | cut -d '"' -f 4|wget -i - -O /root/NadekoBot.latest.zip &&\
+fi
+
 unzip /root/NadekoBot.latest.zip -d /root/NadekoBot
 
 #Sync any new data files
