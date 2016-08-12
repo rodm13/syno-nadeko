22190df132fbc13f56e62b3b095e5e07d90a68bf
Poag <Poag@users.noreply.github.com>
2016-08-12 15:27:59 +0100

    Derp

--- init/40_nadekoinstall.sh
+++ init/40_nadekoinstall.sh
@@ -4,10 +4,10 @@
 rm -rf /root/NadekoBot
 
 #Get NadekoBot Version
-if [[ "$VERSION" = dev]]; then
-	curl -s https://api.github.com/repos/Kwoth/NadekoBot/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4|wget -i - -O /root/NadekoBot.latest.zip &&\
+if [[ "$VERSION" = dev ]]; then
+        curl -s https://api.github.com/repos/Kwoth/NadekoBot/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4|wget -i - -O /root/NadekoBot.latest.zip
 else
-	curl -s https://api.github.com/repos/Kwoth/NadekoBot/releases/latest | grep browser_download_url | head -n 1 | cut -d '"' -f 4|wget -i - -O /root/NadekoBot.latest.zip &&\
+        curl -s https://api.github.com/repos/Kwoth/NadekoBot/releases/latest | grep browser_download_url | head -n 1 | cut -d '"' -f 4|wget -i - -O /root/NadekoBot.latest.zip  &&\
 fi
 
 unzip /root/NadekoBot.latest.zip -d /root/NadekoBot
