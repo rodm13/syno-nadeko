1af2b5417c4d6e2d306532bb8935be01eea7ba8c
Poag <Poag@users.noreply.github.com>
2017-01-09 19:03:30 +0000

    Delete 30_oldconfig.sh

--- init/30_oldconfig.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#Test For Old Style config files
-if [-d /config/data]; then echo "Data dir ok"; else mkdir -p /config/data; fi
-if test -f "/config/config.json"; then mv /config/config.json /config/data/config.json; else echo "New Config"; fi
-if test -f "/config/nadekobot.sqlite"; then mv /config/nadekobot.sqlite /config/data/nadekobot.sqlite; else echo "New SQlite"; fi
