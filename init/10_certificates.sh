f01a991fedd859caff9b2c3c6539bebd21e6abe2
Poag <Poag@users.noreply.github.com>
2017-01-09 19:03:26 +0000

    Delete 10_certificates.sh

--- init/10_certificates.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Import Certificates
-mozroots --import --ask-remove --machine
-echo Y |certmgr --ssl https://gateway.discord.gg /all
-echo Y |certmgr --ssl https://gateway.discord.gg /all
-echo Y |certmgr --ssl https://gateway.discord.gg /all
-echo Y |certmgr --ssl https://discordapp.com /all
