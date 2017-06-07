a0a5afe9dbdf1380cccca0fd9c7819c3143edb4a
Poag <Poag@users.noreply.github.com>
2017-06-07 14:34:01 +0100

    Update nadeko_autorestart.sh

--- nadeko_autorestart.sh
+++ nadeko_autorestart.sh
@@ -14,11 +14,11 @@ echo "Linking Nadeko DB"
 mkdir /root/nadeko
 mv -n /opt/NadekoBot/src/NadekoBot/bin/Release/netcoreapp1.1/data/NadekoBot.db /root/nadeko/NadekoBot.db
 rm /opt/NadekoBot/src/NadekoBot/bin/Release/netcoreapp1.1/data/NadekoBot.db
-ln -s ln -s /root/nadeko/NadekoBot.db /opt/NadekoBot/src/NadekoBot/bin/Release/netcoreapp1.1/data/NadekoBot.db
+ln -s /root/nadeko/NadekoBot.db /opt/NadekoBot/src/NadekoBot/bin/Release/netcoreapp1.1/data/NadekoBot.db
 
 mv -n /opt/NadekoBot/src/NadekoBot/credentials.json /root/nadeko/credentials.json
 rm /opt/NadekoBot/src/NadekoBot/credentials.json
-ln -s ln -s /root/nadeko/NadekoBot.db /opt/NadekoBot/src/NadekoBot/credentials.json
+ln -s /root/nadeko/NadekoBot.db /opt/NadekoBot/src/NadekoBot/credentials.json
 
 cd NadekoBot/src/NadekoBot
 echo "Running NadekoBot with auto restart Please wait."
